efd2e050cc5579109b534238d7b9bba95fe43997
webapptry1231-source <webapptry1231@gmail.com>
2026-04-08 01:20:36 +0530

    Update rpc_manager.py

--- core/rpc_manager.py
+++ core/rpc_manager.py
@@ -3,7 +3,7 @@ import time
 import logging
 from typing import Optional, List, Dict
 from web3 import AsyncWeb3
-from web3.providers import AsyncWebSocketProvider
+from web3.providers import WebsocketProvider
 from web3.providers.async_rpc import AsyncHTTPProvider
 from config import CONFIG, HEALTH_CHECK_INTERVAL_SEC, RPC_MAX_CALLS_SEC
 
@@ -231,7 +231,7 @@ class RPCManager:
             if not ws_endpoint:
                 continue
             try:
-                self._ws_w3 = AsyncWeb3(AsyncWebSocketProvider(ws_endpoint))
+                self._ws_w3 = AsyncWeb3(WebsocketProvider(ws_endpoint))
                 self._ws_current_index = i
                 logger.info(f"Connected to WebSocket RPC: {ws_endpoint}")
                 return self._ws_w3
